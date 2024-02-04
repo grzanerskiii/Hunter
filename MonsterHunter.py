03b59ea86bfce670dac9293d3028749a32157330
Krzysztof <155308399+grzanerskiii@users.noreply.github.com>
2024-02-04 15:54:46 +0100

    Add files via upload

---
+++ MonsterHunter.py
@@ -0,0 +1,93 @@
+import pygame
+import sys
+
+def check_quit(event):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+class Monster:
+    def __init__(self, type):
+        self.type = type
+        if type == "pink":
+            self.speed = 1
+            self.size = 50
+            self.sprite = pygame.image.load('./pictures/monster_pink.png')
+        
+        elif type == "yellow":
+            self.speed = 2
+            self.size = 100
+            self.sprite = pygame.image.load('./pictures/monster_yellow.png')
+        elif type == "red":
+            self.speed = 3
+            self.size = 150
+            self.sprite = pygame.image.load('./pictures/monster_red.png')
+        self.rect = self.sprite.get_rect()
+        self.position = [50, 50]
+        self.rect.center = self.position  
+    
+    def MonsterMove(self,):
+            #self.win = win
+            vector = [2, 0]
+            self.rect = self.sprite.get_rect()
+            self.rect = self.rect.move(vector)
+            if self.rect.left < win.left:
+                vector[0] = -vector[0]
+            elif self.rect.right > win.right:
+                vector[0] = -vector[0]
+            if self.rect.top < win.top or self.rect.bottom > win.bottom:
+                vector[1] = -vector[1]
+
+def createMonsterList():
+    
+    # Initialize Pygame
+pygame.init()
+pygame.mixer.init()
+
+# Setup values
+window_width = 1000
+window_height = 500
+fpsClock = pygame.time.Clock()
+fps = 60
+#background
+game_window = pygame.display.set_mode((window_width, window_height))
+win = game_window.get_rect()
+# monster list
+monsterList = []
+# Load images
+background = pygame.image.load('./pictures/background_hell.jpg')
+pygame.display.set_caption('MonsterHunter')
+pygame.display.set_icon(pygame.image.load('./pictures/monster_red.png'))
+# Setting monster queue
+
+# starting game state
+state = "menu"
+
+
+
+
+
+
+
+def main():
+    # Game loop
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        game_window.blit(background, (0, 0))
+        # Draw the monsters
+        
+        
+    
+
+        # Update the display
+        fpsClock.tick(fps)
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,79 @@
+import pygame
+import sys
+
+def check_quit():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+class Monster:
+    def __init__(self, monster_type, position):
+        self.type = monster_type
+        self.position = position
+        if monster_type == "pink":
+            self.speed = 1
+            self.size = 50
+            self.sprite = pygame.image.load('./pictures/monster_pink.png')
+        elif monster_type == "yellow":
+            self.speed = 2
+            self.size = 100
+            self.sprite = pygame.image.load('./pictures/monster_yellow.png')
+        elif monster_type == "red":
+            self.speed = 3
+            self.size = 150
+            self.sprite = pygame.image.load('./pictures/monster_red.png')
+        self.rect = self.sprite.get_rect()
+        self.rect.center = self.position
+
+def MonsterMove(monster, win):
+    vector = [2, 0]
+    monster.rect = monster.sprite.get_rect()
+    monster.rect = monster.rect.move(vector)
+    if monster.rect.left < win.left or monster.rect.right > win.right:
+        vector[0] = -vector[0]
+    if monster.rect.top < win.top or monster.rect.bottom > win.bottom:
+        vector[1] = -vector[1]
+    monster.position[0] += vector[0]
+    monster.position[1] += vector[1]
+
+def main():
+    pygame.init()
+    pygame.mixer.init()
+
+    window_width = 1000
+    window_height = 500
+    game_window = pygame.display.set_mode((window_width, window_height))
+    win = game_window.get_rect()
+
+    fpsClock = pygame.time.Clock()
+    fps = 60
+
+    background = pygame.image.load('./pictures/background_hell.jpg')
+    pygame.display.set_caption('MonsterHunter')
+    pygame.display.set_icon(pygame.image.load('./pictures/monster_red.png'))
+
+    running = True
+    while running:
+        check_quit()
+
+        game_window.blit(background, (0, 0))
+
+        monster1 = Monster("pink", [50, 50])
+        monster2 = Monster("yellow", [200, 200])
+        monster3 = Monster("red", [400, 400])
+
+        MonsterMove(monster1, win)
+        MonsterMove(monster2, win)
+        MonsterMove(monster3, win)
+
+        # Draw the monsters after updating their positions
+        game_window.blit(monster1.sprite, monster1.position)
+        game_window.blit(monster2.sprite, monster2.position)
+        game_window.blit(monster3.sprite, monster3.position)
+
+        pygame.display.flip()
+        fpsClock.tick(fps)
+
+if __name__ == "__main__":
+    main()
